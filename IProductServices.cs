52354583ebade0b05ef3123c735bd2e0783c113c
PrashantSuryawanshi <98249567+PrashantSuryawanshi@users.noreply.github.com>
2022-04-22 22:33:22 +0530

    Add files via upload

---
+++ IProductServices.cs
@@ -0,0 +1,16 @@
+﻿using SampleWebApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Services
+{
+   public interface IProductServices
+    {
+        IEnumerable<Product> GetAllProducts();
+        int AddProduct(Product prod);
+        int ModifyProduct(Product prod);
+        int DeleteProduct(int id);
+    }
+}
---
+++ ProductServices.cs
@@ -0,0 +1,34 @@
+﻿using SampleWebApi.Model;
+using SampleWebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Services
+{
+    public class ProductServices : IProductServices
+    {
+       private readonly IProductRepositoy _iProductRepo;
+        public ProductServices(IProductRepositoy iProductRepo)
+        {
+            _iProductRepo = iProductRepo;
+        }
+        public int AddProduct(Product prod)
+        {
+            return _iProductRepo.AddProduct(prod);
+        }
+        public int DeleteProduct(int id)
+        {
+            return _iProductRepo.DeleteProduct(id);
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _iProductRepo.GetAllProducts();
+        }
+        public int ModifyProduct(Product prod)
+        {
+            return _iProductRepo.ModifyProduct(prod);
+        }
+    }
+}
